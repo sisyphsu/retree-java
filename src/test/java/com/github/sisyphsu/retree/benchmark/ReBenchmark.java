162db3946fd407ddc2617c2f4172e6f924645add
sulin <sulinixl@163.com>
2019-09-10 21:24:41 +0800

    backup

--- src/test/java/com/github/sisyphsu/retree/benchmark/ReBenchmark.java
+++ src/test/java/com/github/sisyphsu/retree/benchmark/ReBenchmark.java
@@ -17,11 +17,6 @@ import java.util.regex.Pattern;
  * ReBenchmark.regex   avgt    6  12.674 ± 2.923  ns/op
  * ReBenchmark.retree  avgt    6  66.145 ± 1.126  ns/op
  * <p>
- * Find:
- * Benchmark           Mode  Cnt     Score     Error  Units
- * ReBenchmark.regex   avgt    6   219.558 ±   9.406  ns/op
- * ReBenchmark.retree  avgt    6  7183.744 ± 283.702  ns/op
- * <p>
  * Keep using stack for 15% improvement of performance.
  * Before：
  * Benchmark           Mode  Cnt     Score    Error  Units
@@ -37,13 +32,13 @@ import java.util.regex.Pattern;
  */
 @Warmup(iterations = 2, time = 2)
 @BenchmarkMode(Mode.AverageTime)
-@Fork(1)
+@Fork(2)
 @Measurement(iterations = 3, time = 3)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 public class ReBenchmark {
 
     public static final String TEXT = "You have an new email from @Sisyphsu <sisyphsu@gmail.com> and @Sulin <sulin@xxx.com> at 2019-09-07.";
-    public static final String RE = "\\w";
+    public static final String RE = "(\\W)";
 
     public static final Matcher matcher = Pattern.compile(RE).matcher(TEXT);
     public static final ReMatcher reMatcher = new ReMatcher(new ReTree(RE), TEXT);
@@ -52,9 +47,14 @@ public class ReBenchmark {
         int count = 0;
         matcher.reset(TEXT);
 //            matcher.matches();
-        while (matcher.find()) {
-            count++;
+        for (int i = 0; i < TEXT.length(); i++) {
+            if (matcher.find(i)) {
+                count++;
+            }
         }
+//        while (matcher.find()) {
+//            count++;
+//        }
         return count;
     }
 
@@ -62,9 +62,14 @@ public class ReBenchmark {
         int count = 0;
         reMatcher.reset(TEXT);
 //        reMatcher.matches();
-        while (reMatcher.find()) {
-            count++;
+        for (int i = 0; i < TEXT.length(); i++) {
+            if (reMatcher.find(i)) {
+                count++;
+            }
         }
+//        while (reMatcher.find()) {
+//            count++;
+//        }
         return count;
     }
 
